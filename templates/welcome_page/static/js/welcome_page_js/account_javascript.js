6b723cdd226327b063cdd5382f4e3f422b5c1de1
FXCreatives <visionarybusiness.op@gmail.com>
2025-09-03 17:55:52 +0000

    Update account_javascript.js

--- templates/welcome_page/static/js/welcome_page_js/account_javascript.js
+++ templates/welcome_page/static/js/welcome_page_js/account_javascript.js
@@ -4,21 +4,27 @@ const studentChoice = document.getElementById('user_choice2');
 const createAccountLink = document.querySelector('#create_account a');
 const loginLink = document.querySelector('#login a');
 
-const roleChoices = document.getElementsByName('user');
+const ds = document.body.dataset;
+const LECTURER_CREATE = ds.lecturerCreate;
+const LECTURER_LOGIN  = ds.lecturerLogin;
+const STUDENT_CREATE  = ds.studentCreate;
+const STUDENT_LOGIN   = ds.studentLogin;
 
-roleChoices.forEach(radio => {
-    radio.addEventListener('change', () => {
-        if (studentChoice && studentChoice.checked) {
-            createAccountLink.href = 'student_create_account.html';
-            loginLink.href = 'student_login.html';
-        } else if (lecturerChoice && lecturerChoice.checked) {
-            createAccountLink.href = 'lecturer_create_account.html';
-            loginLink.href = 'lecturer_login.html';
-        }
-    });
+function setLinks(role) {
+  if (role === 'student') {
+    createAccountLink.href = STUDENT_CREATE;
+    loginLink.href = STUDENT_LOGIN;
+  } else {
+    createAccountLink.href = LECTURER_CREATE;
+    loginLink.href = LECTURER_LOGIN;
+  }
+}
+
+document.querySelectorAll('input[name="user"]').forEach(radio => {
+  radio.addEventListener('change', () => {
+    setLinks(studentChoice && studentChoice.checked ? 'student' : 'lecturer');
+  });
 });
 
-if (lecturerChoice && lecturerChoice.checked) {
-    createAccountLink.href = 'lecturer_create_account.html';
-    loginLink.href = 'lecturer_login.html';
-}
+// On load, default to lecturer unless student is preselected
+setLinks(studentChoice && studentChoice.checked ? 'student' : 'lecturer');
